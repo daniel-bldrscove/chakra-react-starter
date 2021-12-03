77edf627480a0abe99e91913ef86c0d073b9551b
Daniel Lopez <daniel@bldrscove.com>
2021-12-02 21:05:21 -0500

    Fix react typescript eslint errors.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,9 @@ module.exports = {
   extends: [
     "airbnb-typescript",
     "airbnb/hooks",
+    "eslint:recommended",
     "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:@typescript-eslint/recommended",
     "plugin:jest/recommended",
     "plugin:import/recommended",
@@ -28,6 +30,28 @@ module.exports = {
     "@typescript-eslint",
     "jest",
   ],
+  // temporary workaround to remove typescript no-unused-vars eslint error
+  "overrides": [
+    {
+       files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+          ecmaVersion: 12,
+          sourceType: 'module',
+          ecmaFeatures: {
+              jsx: true,
+          },
+
+          // typescript-eslint specific options
+          warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ['@typescript-eslint'],
+
+      rules: {
+          "@typescript-eslint/no-unused-vars": "off",
+      }
+    }
+  ],
   rules: {
     "prettier/prettier": [
       "warn",
@@ -38,8 +62,19 @@ module.exports = {
     "arrow-body-style": "off",
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".ts", ".jsx", ".tsx",] }],
     "react/state-in-constructor": "off",
+    "react/jsx-uses-vars": 2,
   },
   settings:{
-    "import/ignore": ["node_modules"]
+    "import/ignore": ["node_modules"],
+    "react": {
+      "createClass": "createReactClass", // Regex for Component Factory to use,
+                                         // default to "createReactClass"
+      "pragma": "React",  // Pragma to use, default to "React"
+      "fragment": "Fragment",  // Fragment to use (may be a property of <pragma>), default to "Fragment"
+      "version": "detect", // React version. "detect" automatically picks the version you have installed.
+                           // You can also use `16.0`, `16.3`, etc, if you want to override the detected value.
+                           // default to latest and warns if missing
+                           // It will default to "detect" in the future
+    },
   }
 };
